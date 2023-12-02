9302ced210bc91f14f098a1eea44d646a2d59d5c
ttole123 <ttole0718@gmail.com>
2023-12-02 19:14:50 +0900

    commit

---
+++ twit/hooks/useNotifications.ts
@@ -0,0 +1,18 @@
+/*
+import useSWR from 'swr';
+
+
+const useNotifications = (userId?: string) => {
+  const url = userId ? `/api/notifications/${userId}` : null;
+  const { data, error, isLoading, mutate } = useSWR(url);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate
+  }
+};
+
+export default useNotifications;
+*/
